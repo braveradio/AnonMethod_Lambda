ab9b2e0f4e2b343a426bc9ac6f8ae56818f9ab17
Ильнур Гиззатуллин <juvebars@mail.ru>
2020-01-23 00:00:48 +0300

    Добавьте файлы проекта.

---
+++ AnonMethod_Lambda/Lesson.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonMethod_Lambda
+{
+    class Lesson
+    {
+        public string Name { get; set; }
+        public DateTime Begin { get; private set; }
+        public event EventHandler<DateTime> Started;
+        public Lesson(string name)
+        {
+            // check input arguments
+            Name = name;
+        }
+        public void Start()
+        {
+            Begin = DateTime.Now;
+            Started?.Invoke(this, Begin);
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ AnonMethod_Lambda/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonMethod_Lambda
+{
+    class Program
+    {
+        delegate int MyHandler(int i);
+        static void Main(string[] args)
+        {
+
+            var lesson = new Lesson("Programming in C#");
+            lesson.Started += (sender, date) =>
+            {
+                Console.WriteLine(sender);
+                Console.WriteLine(date);
+            };
+
+            lesson.Start();
+
+
+            if (int.TryParse(Console.ReadLine(), out int result))
+            {
+                MyHandler handler = delegate (int i)
+                {
+                    var r = i * i;
+                    Console.WriteLine(r);
+                    return r;
+                };
+                handler += Method;
+
+                handler(result);
+
+                MyHandler lambdaHandler = (i) =>
+                {
+                    var r = i * i * i;
+                    Console.WriteLine(r);
+                    return r;
+                };
+
+                lambdaHandler(4);
+            }
+            Console.ReadLine();
+        }
+        public static int Method(int i)
+        {
+            var r = i * i * i;
+            Console.WriteLine(r);
+            return r;
+        }
+    }
+}
